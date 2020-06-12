90788075c5a4d4d49ef0c06efbde70a4b9f51c6f
Csaba Csenteri <csaba.csenteri@oracle.com>
2020-06-12 10:53:44 -0400

    create the main.ts boostrap file

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+//boostrap logic for your app
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
